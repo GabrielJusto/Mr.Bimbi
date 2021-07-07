a967987d13dc59c92fca49716080318631a7a3d3
gabrielpaiz <gabriel.paiz@acad.pucrs.br>
2021-07-07 11:20:34 -0300

    Catch funcionado bonitinho e pegar e soltar está no mesmo botão

--- Assets/Scripts/Catch.cs
+++ Assets/Scripts/Catch.cs
@@ -6,6 +6,8 @@ public class Catch : MonoBehaviour
 
     bool catched;
     public GameObject player;
+    public GameObject pickUp;
+
 
 
     // Start is called before the first frame update
@@ -17,25 +19,26 @@ public class Catch : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
-        if(Mathf.Abs(transform.position.x - player.transform.position.x) < 1.2
-            && Mathf.Abs(transform.position.y - player.transform.position.y) <1.2
-            && Input.GetKeyDown("space"))
+
+        if (Mathf.Abs(transform.position.x - player.transform.position.x) < 1
+            && Mathf.Abs(transform.position.y - player.transform.position.y) < 1
+            && Input.GetKeyDown("z") && !catched)
         {
             catched = true;
-            gameObject.GetComponent<BoxCollider2D>().isTrigger = true;
+            this.transform.parent = pickUp.transform;
         }
-        
-        if(catched)
-        { 
-            transform.position = new Vector2(player.transform.position.x - 0.2f, player.transform.position.y - 0.2f);
-            if(Input.GetKeyDown("z"))
+        else 
+        {
+            if (catched)
             {
-                catched = false;
-                gameObject.GetComponent<BoxCollider2D>().isTrigger = false;
+                if (Input.GetKeyDown("z"))
+                {
+                    catched = false;
+                    this.transform.parent = null;
+                    gameObject.GetComponent<BoxCollider2D>().isTrigger = false;
+                }
             }
         }
-
         
     }
 }
